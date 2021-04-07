cef6104e01a3b0fdead3d21d93478392f6575afc
wilson <whuang3015@gmail.com>
2021-04-07 15:49:17 -0700

    Work on implementing player turns

--- connect-four.js
+++ connect-four.js
@@ -1,86 +1,91 @@
-// import { Game } from "./game.js";
-
-import Game from "./game";
+import Game from "./game.js";
 
 let game = undefined;
-let updateUI = function () {
-    const boardHolder = window.getElementById("board-holder");
-    const gameName = window.getElementById("game-name");
 
-    if (game === undefined) {
-        boardHolder.classList.add("is-invisible");
-    } else {
-        boardHolder.classList.remove("is-invisible");
-        gameName.innerHTML = getName();
-    }
-}
+let updateUI = function () {
+  const topBar = document.getElementById("click-targets");
+  const boardHolder = document.getElementById("board-holder");
+  const gameName = document.getElementById("game-name");
+
+  if (game === undefined) {
+    boardHolder.classList.add("is-invisible");
+  } else {
+    boardHolder.classList.remove("is-invisible");
+    gameName.innerHTML = game.getName();
+  }
+
+  if (game.currentPlayer === 1) {
+    topBar.classList.add('black');
+    topBar.classList.remove('red');
+  }
+
+};
 
 window.addEventListener("DOMContentLoaded", (event) => {
-    const form = document.getElementById("form-holder");
-    const firstPlayer = document.getElementById("player-1-name");
-    const secondPlayer = document.getElementById("player-2-name");
-    const newGameButton = document.getElementById("new-game");
+  const form = document.getElementById("form-holder");
+  const firstPlayer = document.getElementById("player-1-name");
+  const secondPlayer = document.getElementById("player-2-name");
+  const newGameButton = document.getElementById("new-game");
 
-    form.addEventListener("keyup", (event) => {
+  form.addEventListener("keyup", (event) => {
+    if (firstPlayer.value && secondPlayer.value) {
+      newGameButton.removeAttribute("disabled");
+    }
+    if (!firstPlayer.value || !secondPlayer.value) {
+      newGameButton.setAttribute("disabled", null);
+    }
+  });
 
-        if (firstPlayer.value && secondPlayer.value) {
-            newGameButton.removeAttribute("disabled");
-        }
-        if (!firstPlayer.value || !secondPlayer.value) {
-            newGameButton.setAttribute("disabled", null);
-        }
-    });
+  newGameButton.addEventListener("click", (event) => {
+    game = new Game(firstPlayer.value, secondPlayer.value);
+    updateUI();
+  });
 
-    newGameButton.addEventListener("click", event => {
-        game = new Game(firstPlayer.value, secondPlayer.value);
-        updateUI();
-    })
+  const topBar = document.getElementById("click-targets");
+  topBar.addEventListener("click", event => {
+    game.playInColumn();
+    updateUI();
+  })
 
-
-
-    //const
-    //
-    //let playerTurn = "black";
-    //
-    //// create a game class
-    //// and a column class
-    //
-    //// Logic planning
-    //
-    //const clickTargets = document.querySelectorAll('.click-target');
-    //console.log(clickTargets);
-    ////clickTargets.forEach(target => {
-    ////    target.addEventListener('mouseenter', event => {
-    ////        // Put column full logic here
-    ////        event.target.classList.add('full');
-    ////    })
-    ////    target.addEventListener('mouseleave', event => {
-    ////        // Put column full logic here
-    ////        event.target.classList.remove('full');
-    ////    })
-    ////})
-    //
-    //const clickTargetTurn = document.getElementById("click-targets");
-    //clickTargetTurn.addEventListener("mouseenter", event => {
-    //    event.target.classList.add('black');
-    //})
-    //
-    //clickTargets.forEach(target => {
-    //    target.addEventListener('click', event => {
-    //        // match the click target's column number with the board square's column
-    //        let targetColumn = event.target.id[event.target.id.length - 1];
-    //        let targetSquare = document.getElementById(`square-5-${targetColumn}`);
-    //        let newDiv = document.createElement('div');
-    //        newDiv.classList.add('token', 'black');
-    //        // newDiv.setAttribute('class', 'black');
-    //        targetSquare.appendChild(newDiv);
-    //        // console.log()
-    //    })
-    //})
-    //
-    //const boardSquares = document.getElementById("board-squares");
-    //
-    //
-    //
-    //
+  //const
+  //
+  //let playerTurn = "black";
+  //
+  //// create a game class
+  //// and a column class
+  //
+  //// Logic planning
+  //
+  //const clickTargets = document.querySelectorAll('.click-target');
+  //console.log(clickTargets);
+  ////clickTargets.forEach(target => {
+  ////    target.addEventListener('mouseenter', event => {
+  ////        // Put column full logic here
+  ////        event.target.classList.add('full');
+  ////    })
+  ////    target.addEventListener('mouseleave', event => {
+  ////        // Put column full logic here
+  ////        event.target.classList.remove('full');
+  ////    })
+  ////})
+  //
+  //
+  //clickTargets.forEach(target => {
+  //    target.addEventListener('click', event => {
+  //        // match the click target's column number with the board square's column
+  //        let targetColumn = event.target.id[event.target.id.length - 1];
+  //        let targetSquare = document.getElementById(`square-5-${targetColumn}`);
+  //        let newDiv = document.createElement('div');
+  //        newDiv.classList.add('token', 'black');
+  //        // newDiv.setAttribute('class', 'black');
+  //        targetSquare.appendChild(newDiv);
+  //        // console.log()
+  //    })
+  //})
+  //
+  //const boardSquares = document.getElementById("board-squares");
+  //
+  //
+  //
+  //
 });
--- game.js
+++ game.js
@@ -3,9 +3,18 @@ export default class Game {
     constructor(player1, player2) {
         this.player1 = player1;
         this.player2 = player2;
+        this.currentPlayer = 1;
     }
 
     getName() {
-        return `${player1} vs. ${player2}`;
+        return `${this.player1} vs. ${this.player2}`;
+    }
+
+    playInColumn() {
+        if (this.currentPlayer === 1) {
+            this.currentPlayer = 2;
+        } else {
+            this.currentPlayer = 1;
+        }
     }
 }
