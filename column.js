f3511391909f0d12c5aff257da5ac706dc5ccd1a
wilson <whuang3015@gmail.com>
2021-04-08 08:32:29 -0700

    Work on Column class

--- column.js
+++ column.js
@@ -1,13 +1,36 @@
 export default class Column {
-    constructor(depth) {
-        this.depth = depth;
+    constructor(array = [null, null, null, null, null, null]) {
+        this.array = array
+
     }
 
-    add() {
+    add() { // stores the player in the bottom-most slot in the column
+        
+        for (let i = this.array.length - 1; i >= 0; i--) {
+            if (this.array[i] === null) {
+                this.array[i] = game.currentPlayer;
+            }
 
+        }
     }
 
     getTokenAt(row) {
 
     }
 }
+
+
+// column1 = {key: []} // as we add tokens, unshift new things in
+
+// column2 = { 0: null,  1: null, 2: 2, } // <=
+
+// new Column([])
+
+// const column0 = new Column()
+// column1.array[0] = '1'
+// const column1 = new Column()
+// const column2 = new Column()
+// const column3 = new Column()
+// const column4 = new Column()
+// const column5 = new Column()
+// const column6 = new Column()
