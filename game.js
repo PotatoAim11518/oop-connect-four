b8e10ab08cad2c39f12c1365ed505a12ef128c6b
wilson <whuang3015@gmail.com>
2021-04-08 15:00:12 -0700

    Implement row and column win inspectors

--- game.js
+++ game.js
@@ -1,4 +1,6 @@
 import Column from "./column.js";
+import ColumnWinInspector from "./column-win-inspector.js";
+import RowWinInspector from "./row-win-inspector.js";
 
 export default class Game {
 
@@ -12,16 +14,15 @@ export default class Game {
         for (let i = 0; i < 7; i++) {
             this.columns.push(new Column());
         }
-
-        const topBar = document.getElementById("click-targets");
-        topBar.addEventListener("click", event => {
-            let colIndex = event.target.id.slice("-")[1];
-        })
     }
 
     getName() {
         if (this.winnerNumber === 3) {
             return `${this.player1} ties with ${this.player2}!`;
+        } else if (this.winnerNumber === 2) {
+            return `${this.player2} wins!`;
+        } else if (this.winnerNumber === 1) {
+            return `${this.player1} wins!`;
         } else {
             return `${this.player1} vs. ${this.player2}`;
         }
@@ -37,6 +38,35 @@ export default class Game {
         return true;
     }
 
+    checkForColumnWin() {
+        for (let column of this.columns) {
+            const columnUnderInspection = new ColumnWinInspector(column);
+            if (columnUnderInspection.inspect() === 1 || columnUnderInspection.inspect() === 2){
+                this.winnerNumber = columnUnderInspection.inspect();
+                return
+            }
+        }
+    }
+
+    checkForRowWin() {
+        const colGroup_0_3 = this.columns.slice(0,4); // array of 4 Column objects containing an array of 6 values
+        const colGroup_1_4 = this.columns.slice(1,5);
+        const colGroup_2_5 = this.columns.slice(2,6);
+        const colGroup_3_6 = this.columns.slice(3);
+
+        const colSlices = [colGroup_0_3, colGroup_1_4, colGroup_2_5, colGroup_3_6]
+
+        for (let slice of colSlices) {
+            let inspectSlice = new RowWinInspector(slice);
+            console.log(inspectSlice.inspect())
+            if (inspectSlice.inspect() > 0) {
+                this.winnerNumber = inspectSlice.inspect();
+                break;
+            }
+        }
+    }
+
+
     playInColumn(col) {
         this.columns[col].add(this.currentPlayer);
         if (this.currentPlayer === 1) {
@@ -44,7 +74,16 @@ export default class Game {
         } else {
             this.currentPlayer = 1;
         }
+
         this.checkForTie();
+
+        if (this.winnerNumber !== 0) {
+            return
+        } else {
+            this.checkForColumnWin();
+        }
+        this.checkForRowWin();
+
     }
 
     getTokenAt(row, col) {
@@ -53,6 +92,10 @@ export default class Game {
     }
 
     isColumnFull(col) {
+        if (this.winnerNumber === 1 || this.winnerNumber === 2) {
+            return true;
+        }
         return this.columns[col].isFull()
     }
+
 }
---
+++ row-win-inspector.js
@@ -0,0 +1,25 @@
+import Column from "./column.js";
+
+export default class RowWinInspector {
+  constructor(columns) {
+    this.columns = columns;
+
+    // for (let i = 0; i < 4; i++) {
+    //   const colObj = new Column();
+    //   this.columns.push(colObj);
+    // }
+  }
+
+  inspect() {
+    for (let row = 0; row < 6; row++) {
+      let col0 = this.columns[0].getTokenAt(row);
+      let col1 = this.columns[1].getTokenAt(row);
+      let col2 = this.columns[2].getTokenAt(row);
+      let col3 = this.columns[3].getTokenAt(row);
+      if (col0 === col1 && col1 === col2 && col2 === col3 && col0 !== null) {
+        return col0;
+      }
+    }
+    return 0;
+  }
+}
